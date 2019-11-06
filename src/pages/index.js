792d14e906cf354369c7c45f0ea362f5f542de98
Gary Henderson <hendergy@gmail.com>
2019-11-06 13:40:23 -0500

    something changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Test from "../components/test"
-import NewHeader from "../components/NewHeader"
 
 const IndexPage = () => (
   <Layout>
