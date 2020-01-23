9849a3bb03d679dd603357db6a92618cfdc7db08
Gary Henderson <hendergy@gmail.com>
2020-01-22 19:24:04 -0500

    updated Navigation component to display Navbar

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import TopNav from "./topnav"
-import SideNav from "./sidenav"
+import NavBar from "./navbar"
 
 import navStyles from "../nav/navigation.module.css"
-import sidenavStyles from "../nav/sidenav.module.css"
 
 const Navigation = (props) => {
-    let width = window.innerWidth < 450;
 
-    if (width) {
-        return <TopNav style={sidenavStyles} />
-    }
-
-    return <TopNav style={navStyles} />
+    return (
+        <NavBar style={navStyles} />
+    )
 }
 
 export default Navigation;
