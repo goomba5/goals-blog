ec4e230bc9135552a35e3d6aac2ad3d409f6de2b
Gary Henderson <hendergy@gmail.com>
2020-01-26 23:52:24 -0500

    created CSS module with flex styles for About component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,51 @@
-import React, { Component } from 'react';
-import Layout from "../components/layout";
+import React from 'react';
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-class About extends Component {
-    render() {
-        return (
-            <Layout>
-                <div>
-                    <h1>
-                        About
-                </h1>
-                    <p>This is a placeholder for the About page that will created for this site.</p>
-                    <p>So, just hang tight. We'll get to this soon!</p>
+import Layout from "../components/layout";
+import about from "../components/about.module.css"
+
+export const data =
+    graphql`
+        query{
+            file(relativePath: {eq:"18ZG_Thurs-029.jpg"}){
+                childImageSharp{
+                    fluid {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }`
+
+const About = ({ data }) => {
+
+    return (
+        <Layout>
+            <div className={about.flexContainer}>
+                <div className={about.flexItem}>
+                    <h2 className={about.title}>Who I Am</h2>
+                    <p>
+                        Yo! My name is Gary and I am an aspiring web developer. For the past couple of years, I have been on a winding journey to learn as much as possible about all of the tools of the trade, and to collect as many experiences as possible. This path has required a lot of time, practice, patience, and, most importantly, persistence. At the core of each experience - especially those in the future - there are two things I always make sure of, and those are to have fun and enjoy yourself.
+            </p>
                 </div>
-            </Layout>
-        );
-    }
+
+                <div className={about.flexItem}>
+                    <h2 className={about.title}>Where I've Been</h2>
+                    <p>
+                        My name is Gary and I have been a part of Zillow Group for over four years. Prior to my current job, I have been: a logistics team member for a major retailer, a
+            </p>
+                </div>
+
+                <div className={about.flexItem}>
+                    <h2 className={about.title}>Where I'm Going</h2>
+                    <p>
+                        My name is Gary and I have been a part of Zillow Group for over four years. Prior to my current job, I have been: a logistics team member for a major retailer, a
+            </p>
+                </div>
+            </div>
+            <Img className={about.image} alt="Dotloop ZG18 Group Picture" fluid={data.file.childImageSharp.fluid} />
+        </Layout >
+    );
 }
 
 export default About;
