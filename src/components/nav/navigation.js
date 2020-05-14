9e03aa86c1a15084219b7fa187fe479b2e9e2119
Gary Henderson <hendergy@gmail.com>
2020-05-13 21:53:35 -0400

    renamed NavBar to MenuList for consistency

--- src/components/nav/navigation.js
+++ src/components/nav/navigation.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import NavBar from "./navbar"
+import MenuList from "./menuList"
 
 import navStyles from "../nav/navigation.module.css"
 
 const Navigation = (props) => {
 
     return (
-        <NavBar style={navStyles} />
+        <MenuList style={navStyles} />
     )
 }
 
