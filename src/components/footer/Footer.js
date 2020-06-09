829baebdea9629d8342b713a42ef869a6c3f0da8
Gary Henderson <hendergy@gmail.com>
2020-06-08 23:36:47 -0400

    created the Footer component and imported to index file

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h2>Here's a Footer.</h2>
+        </div>
+    )
+}
+
+export default Footer;
