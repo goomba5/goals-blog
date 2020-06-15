a7720774a917ec028e3c20c2692edf12de022df4
Gary Henderson <hendergy@gmail.com>
2020-06-14 21:06:27 -0400

    added reverseEdges function, updated map method to reflect new array

--- src/components/LatestPosts.js
+++ src/components/LatestPosts.js
@@ -3,12 +3,27 @@ import { useLatestPosts } from "../components/queries/LatestPostsQuery"
 import { Link } from "gatsby"
 
 const LatestPosts = () => {
-    const { edges } = useLatestPosts();
+    let { edges } = useLatestPosts();
+
+    const reverseEdges = () => {
+        let latestPosts = [];
+        let edgesLength = edges.length;
+        let postLimit = 4;
+        let counter = 1;
+
+        while (counter < postLimit) {
+            latestPosts.push(edges[edgesLength - counter]);
+            counter++;
+        }
+        return latestPosts;
+    }
+
+    const latestPostList = reverseEdges();
 
     return (
         <div className="homepage__latest-posts-container">
             <div className="homepage__latest-posts">
-                {edges.map(post => {
+                {latestPostList.map((post, index) => {
                     return <div className="homepage__post" key={post.node.id}>
                         <Link to={post.node.fields.slug}>
                             <h3 className="homepage__post-title">{post.node.frontmatter.title}</h3>
