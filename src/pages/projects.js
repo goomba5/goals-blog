e45ff4e12457a7d5219816807ec13fe1e28bc41e
Gary Henderson <hendergy@gmail.com>
2020-06-16 18:42:52 -0400

    import Projects CSS module, updated class names

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import projectStyles from "../components/projects.module.css"
 
 const Projects = () => {
     return (
         <Layout>
-            <div>
-                <h1>Projects</h1>
+            <div className={projectStyles.projectsPage}>
+                <h1 className={projectStyles.projects__projectsTitle}>Projects</h1>
             </div>
         </Layout>
     )
