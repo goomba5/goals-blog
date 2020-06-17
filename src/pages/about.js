162de2d037fb3fbbe48be0312693c24d05536600
Gary Henderson <hendergy@gmail.com>
2020-06-16 21:55:46 -0400

    updated About component class names and CSS module

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,9 +21,9 @@ const About = ({ data }) => {
 
     return (
         <Layout>
-            <div className={about.flexContainer}>
-                <div className={about.flexItem}>
-                    <h2 className={about.title}>What It Do</h2>
+            <div className={about.about_container}>
+                <div className={about.about__section}>
+                    <h2 className={about.about__sectionTitle}>What It Do</h2>
                     <p>
                         Yo! If you are reading this, then congratulations on finding my site - it's not out in the open, that's for sure. With that being said, my name is Gary and I made this site using Gatsby and Netlify CMS.
             </p>
@@ -32,15 +32,15 @@ const About = ({ data }) => {
             </p>
                 </div>
 
-                <div className={about.flexItem}>
-                    <h2 className={about.title}>Where I've Been</h2>
+                <div className={about.about__section}>
+                    <h2 className={about.about__sectionTitle}>Where I've Been</h2>
                     <p>
                         Prior to where I currently work, I was able to work in a number of different roles with varying degrees of responsibilities. I've worked in retail logistics, as a backline cook for a local restaurant, a sales associate and store manager, as well as a service coordinator overseeing technicians who fixed high-end dental equipment. My path has zigged and zagged and culminated in where I am and where I want to go.
             </p>
                 </div>
 
-                <div className={about.flexItem}>
-                    <h2 className={about.title}>Where I'm Going</h2>
+                <div className={about.about__section}>
+                    <h2 className={about.about__sectionTitle}>Where I'm Going</h2>
                     <p>
                         For the past three years, I have been on a journey to completely reset my career from customer service to software development. This path has required a lot of time, practice, patience, and persistence. The journey has had its low points, but I can say with absolute confidence that it has been more than rewarding.
             </p>
