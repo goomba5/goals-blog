77b1f71d421b372c8b3888da128545f31697dd29
Gary Henderson <hendergy@gmail.com>
2020-06-22 07:42:12 -0400

    imported Contact module for styles, added class names to elements

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,13 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import contactStyles from "../components/contact.module.css"
 
 const Contact = () => {
     return (
         <Layout>
-            <div>
-                <h1>Contact</h1>
+            <div className={contactStyles.contact_page}>
+                <h1 className={contactStyles.contact__title}>Contact</h1>
             </div>
         </Layout>
     )
