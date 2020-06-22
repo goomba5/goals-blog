c30010023c7e6a23268f15abae3d8ece7dd548df
Gary Henderson <hendergy@gmail.com>
2020-06-22 00:06:52 -0400

    removed graphql query in place of importing query hook, added function to create descending blog list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,51 +2,48 @@ import React from "react"
 
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
-import flex from "../components/blog.module.css"
+import blogStyles from "../components/blog.module.css"
+import Posts, { useLatestPosts } from "../components/queries/PostsQuery"
 
-export const data =
-    graphql`
-        query {
-            allMarkdownRemark {
-                totalCount
-                edges {
-                    node {
-                        id
-                        fields{
-                            slug
-                        }
-                        wordCount{
-                            words
-                        }
-                        frontmatter {
-                            date(formatString: "MMMM DD, YYYY")
-                            title
-                            description
-          }
-          rawMarkdownBody
-          id
+
+const Blog = () => {
+    const { edges } = useLatestPosts();
+    console.log(edges);
+
+    const reverseEdges = () => {
+        let allPosts = [];
+        let edgesLength = edges.length;
+        let postLimit = 0;
+        let counter = 1;
+
+        while (counter <= edgesLength) {
+            allPosts.push(edges[edgesLength - counter]);
+            counter++;
         }
-      }
+        return allPosts;
     }
-  }`
 
-const Blog = ({ data }) => (
-    <Layout>
-        <h1>Blog Posts</h1>
-        <div className={flex.flexContainer}>
-            {
-                data.allMarkdownRemark.edges.map(({ node }) => (
-                    <Link key={node.id} to={node.fields.slug} className={flex.flexItem}>
-                        <h2>{node.frontmatter.title}</h2>{" "}
-                        <h3>{node.frontmatter.date}</h3>
-                        <p>Total number of words: {node.wordCount.words}</p>
+    let posts = reverseEdges();
+    console.log(posts);
 
-                        <p>{node.frontmatter.description}</p>
-                    </Link>
-                ))
-            }
-        </div>
-    </Layout>
-)
+    return (
+        <Layout>
+            <div className={blogStyles.blog__post_container}>
+                <h1 className={blogStyles.blog__title}>Blog Posts</h1>
+                {
+                    posts.map((post, index) => (
+                        <Link key={index} to={post.node.fields.slug} className={blogStyles.blog__post}>
+                            <h2 className={blogStyles.blog__post_title}>{post.node.frontmatter.title}</h2>{" "}
+                            <h3 className={blogStyles.blog__post_date}>{post.node.frontmatter.date}</h3>
+                            <p className={blogStyles.blog__post_total_words}>Total number of words: {post.node.wordCount.words}</p>
+
+                            <p className={blogStyles.blog__post_description}>{post.node.frontmatter.description}</p>
+                        </Link>
+                    ))
+                }
+            </div>
+        </Layout>
+    )
+}
 
 export default Blog;
